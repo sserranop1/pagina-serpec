88c31c90cae5111abb98f68059512e88bb7d2aaf
Santiago Serrano <s.serranop2@uniandes.edu.co>
2026-06-18 11:18:16 -0500

    primer prototipo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { ContactPage } from './pages/contact-page/contact-page';
+import { Proyectos } from './pages/proyectos/proyectos';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'nosotros', component: About },
+  { path: 'contacto', component: ContactPage },
+  { path: 'proyectos', component: Proyectos },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/proyectos/proyectos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos',
+  imports: [],
+  templateUrl: './proyectos.html',
+  styleUrl: './proyectos.css',
+})
+export class Proyectos {}
